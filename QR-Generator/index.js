2b3b63dd2dbf4880f865075df23be224b635e471
Akshith_V <127883963+Tagcod3r@users.noreply.github.com>
2024-12-19 22:13:57 +0530

    Update index.js

--- QR-Generator/index.js
+++ QR-Generator/index.js
@@ -8,7 +8,7 @@ import fs from 'fs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.port||3000;
+const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
